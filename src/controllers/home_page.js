08b472b80fb5ccd3aa66077370706a520ccf675d
josemnr <maninari97@gmail.com>
2021-03-18 00:49:55 -0600

    Delete unnecessary controller

--- src/controllers/home_page.js
+++ src/controllers/home_page.js
@@ -10,11 +10,6 @@ const toneAnalyzer = new ToneAnalyzerV3({
   serviceUrl: config.serviceUrl,
 });
 
-//Método para render de la página del Adder
-// exports.renderHomePage = (req, res) => {
-//     res.render("index");
-// }
-
 exports.sendData = (req, res) => {
     console.log(req.body.entry)
     let text = req.body.entry
